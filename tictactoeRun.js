6ed59f8b29ed3913fae1d56a2ff00666d0b9e749
yuval gofer <yuvyael@gmail.com>
2021-11-28 20:36:57 +0200

    Creating and modified tictactoeRun.js for ending the game!

--- tictactoeRun.js
+++ tictactoeRun.js
@@ -1,70 +1,89 @@
-const statusDisplay = document.getElementById('gameStatusTag');
+var statusDisplay = document.getElementById('gameStatusTag');
 //Game condition
 let gameActive = true;
 let currentPlayer = "X";
 let gameSelectionArray = ["", "", "", "", "", "", "", "", "",];
 //Game condition
-const winningMessage = () => 'Player ${currentPlayer} has won!';
+var winningMessage = () => `Player ${currentPlayer} has won!`;
 const drawMessage = () => 'Game ended in a draw!';
-const currentPlayerTurn = () => ' It ${currentPlayer} turn'; 
-const winningCondition = [ [0, 1, 2], [3, 4, 5], [6, 7, 8],
-                           [0, 3, 6], [1, 4, 7], [2, 5, 8], 
-                           [0, 4, 8], [2, 4, 6]
-                         ];
+var currentPlayerTurn = () => `It ${currentPlayer} turn`;
+const winningCondition = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
+[0, 3, 6], [1, 4, 7], [2, 5, 8],
+[0, 4, 8], [2, 4, 6]
+];
 //Game status
-statusDisplay.innerHTML = currentPlayerTurn;
+statusDisplay.innerHTML = currentPlayerTurn();
 //Game function
-function squarePlayed(clickedSquare, clickedSquareIndex){
+function squarePlayed(clickedSquare, clickedSquareIndex) {
     gameSelectionArray[clickedSquareIndex] = currentPlayer;
     clickedSquare.innerHTML = currentPlayer;
 }
 function playerChange() {
     currentPlayer = currentPlayer === "X" ? "O" : "X";
-    statusDisplay.innerHTML = currentPlayerTurn;
+    statusDisplay.innerHTML = currentPlayerTurn();
 }
 function resultValidation() {
     let ruondWon = false;
-    for(let i = 0; i <= 9; i++){
-        const winCondition = winningCondition[i];
-        let a = gameSelectionArray[winCondition[0]];
-        let b = gameSelectionArray[winCondition[1]];
-        let c = gameSelectionArray[winCondition[2]];
-    if(a === 'b' || b === 'b' || c === 'b') {
-        continue;
-      }
-    if(a === b && b === c) {
-        ruondWon = true;
-        break
-      }
-    }
-    if(ruondWon) {
-        statusDisplay.innerHTML = winningMessage();
-        gameActive = false;
-        return;
-    }
-        let roundDraw = !gameSelectionArray.includes("");
-        if(roundDraw) {
-            statusDisplay.innerHTML = drawMessage();
-            gameActive = false;
-            return;
+
+    for (let i = 0; i <= 7; i++) {
+        if (gameSelectionArray[winningCondition[i][0]] == gameSelectionArray[winningCondition[i][1]] &&
+            gameSelectionArray[winningCondition[i][1]] == gameSelectionArray[winningCondition[i][2]] &&
+            gameSelectionArray[winningCondition[i][2]] == currentPlayer) {
+                gameActive = false;
+                statusDisplay.innerHTML = winningMessage();
+                return false;
         }
+    }
+
+    return true;
+
+    // for(let i = 0; i <= 9; i++){
+    //     const winCondition = winningCondition[i];
+    //     let a = gameSelectionArray[winCondition[0]];
+    //     let b = gameSelectionArray[winCondition[1]];
+    //     let c = gameSelectionArray[winCondition[2]];
+    // if(a === currentPlayer || b === currentPlayer || c === currentPlayer) {
+    //     continue;
+    //   }
+    // if(a === b && b === c) {
+    //     ruondWon = true;
+    //     break
+    //   }
+    // }
+    // if(ruondWon) {
+    //     statusDisplay.innerHTML = winningMessage();
+    //     gameActive = false;
+    //     return;
+    // }
+    //     let roundDraw = !gameSelectionArray.includes("");
+    //     if(roundDraw) {
+    //         statusDisplay.innerHTML = drawMessage();
+    //         gameActive = false;
+    //         return;
+    //     }
 }
 function squareClick(clickedSquareEvent) {
-    const clickedSquare = clickedSquareEvent.target;
-    const clickedSquareIndex = parseInt(clickedSquare.getAttribute('b'));
-    if(gameSelectionArray[clickedSquareIndex] !== "" || !gameActive){
+
+    var clickedSquare = clickedSquareEvent.target;
+    //clickedSquare.innerText  = currentPlayer;
+    var clickedSquareIndex = clickedSquare.id['1'];
+    //gameSelectionArray[clickedSquare.id['1']] = currentPlayer;
+
+    if (gameSelectionArray[clickedSquareIndex] !== "" || !gameActive) {
         return;
     }
+
     squarePlayed(clickedSquare, clickedSquareIndex);
-    resultValidation();
+    if(resultValidation())
+        playerChange();
 }
-function restartGame(){
+function restartGame() {
     gameActive = true;
     currentPlayer = "X";
     gameSelectionArray = ["", "", "", "", "", "", "", "", "",];
     statusDisplay.innerHTML = currentPlayerTurn();
-    document.querySelectorAll('b').forEach('b'.addEventListener('click', squareClick));
+    document.querySelectorAll('#board > button').forEach((b) => b.addEventListener('click', squareClick));
 }
-//Button query
-document.querySelectorAll('b').forEach('b'.addEventListener('click', squareClick));
+//Button query 
+document.querySelectorAll('#board > button').forEach((b) => b.addEventListener('click', squareClick));
 document.querySelector('header button').addEventListener('click', restartGame);
--- tictactoeUI.js
+++ tictactoeUI.js
@@ -4,8 +4,8 @@ enter code to handle:
 2. game status update messages
 3. Start button click
 */
-const player = ['X', 'O'];
-const cellTag = 'button';
+/*const player = ['X', 'O'];
+const squareTag = 'button';
 const maximumTurns = 10;
 const squareIdPre = 'b';
 const selectionArray = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ];
@@ -27,35 +27,25 @@ function switchPlayers(){
         return player[1];
     }
 }
-const getRandomPlayer = () => player[Math.floor(Math.random() * player.length)];
-const isDraw = () => turnsNumber == maximumTurns;
-const getSquareNum = (squareID) =>parseInt(squareID.replace(squareIdPre, ' '));
+const isDraw = () => turnsNumber == maximumTurns; 
+const getSquareNum = (squareID) => parseInt(squareID.replace(squareIdPre, ''));
+
 //main function
 function startGame() {
-    playerTurn = getRandomPlayer();
     let board = document.getElementById('board');
-    let cellTags = board.getElementsByTagName(cellTag);
+    let squareTags = board.getElementsByTagName(squareTag);
     //
     isGameOver = false;
-    playerTurn = 0;
+    turnsNumber = 0;
 
     setGameStatus();
     document.querySelector('header button').removeEventListener('click', startGame);
     document.querySelector('header button').addEventListener('click', startGame, false);
 
-<<<<<<< HEAD
-    for (let i = 0; i < cellTags.length; i++) {
-       cellTags[i].removeEventListener('click', selectSquare);
-       cellTags[i].addEventListener('click', selectSquare, false);
-       cellTags[i].innerText = ' ';
-       selectionArray[i] = ' ';
-=======
-    for(i = 0; i< cellTags.length; i++){
-    cellTags[i].removeEventListener('click', selectCell);
-    cellTags[i].addEventListener('click', selectCell, false);
-    cellTags[i].innerText = ' ';
-    selectionArray[i] = ' ';
->>>>>>> c340215070ecd6b271076c3cb331bd11c70d3e64
+    for(let i = 0; i< squareTags.length; i++){
+    squareTags[i].removeEventListener('click', selectSquare);
+    squareTags[i].addEventListener('click', selectSquare, false);
+    squareTags[i].innerText = ' ';
     }
 }
 
@@ -69,11 +59,11 @@ function selectSquare(event) {
       }
       isGameOver = hasWon(selectionArray);
     }
-     setGameStatus();
+     /*setGameStatus();*/
 
 //
-function setGameStatus() {
-    if(isGameOver)
+/*function setGameStatus() {
+    if(isGameOver) 
     gameStatusMessage = playerTurn + 'Won the game!';
     else if(turnsNumber == 0)
     gameStatusMessage = 'Waiting for you to start the game..';
@@ -84,8 +74,7 @@ function setGameStatus() {
         gameStatusMessage = 'Player' + playerTurn + 'turn..';
     }
     document.getElementById('gameStatusTag').innerText = gameStatusMessage;
-}
+}/*
     
     
     
-
